d691be267e197042068d509d953be2f38070a750
Carol <ikkilink@hotmail.com>
2024-06-12 18:20:11 -0600

    Updated ScriptableObjs as seen in the video
    We're gonna work PowerUps as seen in the "A great Way to Setup POWERUPS in your Unity Game" video

---
+++ Assets/Scripts/ScriptableObjPowerUps/ScriptableObjPower.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class ScriptableObjPower : ScriptableObject
+{
+    public abstract void Apply(GameObject target);
+  
+}
