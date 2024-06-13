0e2a0237616de9acbcf18d966af5b80ab53415f1
Wilbert <114256884+warcilac9@users.noreply.github.com>
2024-06-13 13:19:59 -0600

    Movimiento listo

--- Assets/Scripts/DebugLevelScripts/Player/PlayerMovement.cs
+++ Assets/Scripts/DebugLevelScripts/Player/PlayerMovement.cs
@@ -5,28 +5,73 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    PlayerInput playerInput;
-    InputAction moveAction;
+    public Rigidbody rb;
 
-    [SerializeField] float speed = 5f;
+    public Transform cam;
+
+    public float horizontal;
+    public float vertical;
+
+    public float initialspeed;
+    float speed;
+    public float scalingspeed;
+
+
+    public float smoothtime = 0.1f;
+    float turnSmoothVelocity;
+
+    private Vector3 previousPosition; //the previous position of the gameobject
+    private Vector3 velocity; //the velocity vector of the gameobject
+    private Vector3 yAxis; //the y axis vector
+
     // Start is called before the first frame update
     void Start()
     {
-        playerInput=GetComponent<PlayerInput>();
-        moveAction = playerInput.actions.FindAction("Move");
-
+        previousPosition = transform.position; //initialize the previous position
+        yAxis = new Vector3(0, 1, 0); //initialize the y axis vector
+        speed = initialspeed;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        MovePlayer();
+        velocity = transform.position - previousPosition; //calculate the velocity vector
+        previousPosition = transform.position; //update the previous position
+
+        float dot = Vector3.Dot(velocity, yAxis); //calculate the dot product
+
+        if (dot > 0.01f)
+        {
+            Debug.Log("Moving upwards");
+            speed = scalingspeed;
+
+        }
+        else if (dot == 0)
+        {
+            speed = initialspeed;
+        }
+
+        Vector3 direction = new Vector3(horizontal * speed, rb.velocity.y * 0, vertical * speed);
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetangle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnSmoothVelocity, smoothtime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 movedir = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
+            rb.velocity = movedir.normalized * speed;
+            rb.velocity = new Vector3(rb.velocity.x, -1.2f, rb.velocity.z);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+
+
     }
 
-    void MovePlayer()
+    public void Move(InputAction.CallbackContext context)
     {
-       Vector2 direction = moveAction.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+        horizontal = context.ReadValue<Vector2>().x;
+        vertical = context.ReadValue<Vector2>().y;
     }
-
 }
