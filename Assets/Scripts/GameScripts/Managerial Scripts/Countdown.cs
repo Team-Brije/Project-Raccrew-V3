bcb94cae9451f566ff9e079028765475d805b18a
Shika <shikatastrophe@gmail.com>
2024-06-21 20:53:31 -0600

    Created Timer and Countdown Text

---
+++ Assets/Scripts/GameScripts/Managerial Scripts/Countdown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    public int duration = 60;
+    public int timeRemaining;
+    public bool isCountingDown = false;
+    public TextMeshProUGUI timerText;
+
+    private void Start()
+    {
+        timerText.text = duration.ToString();
+        Begin();
+    }
+
+    public void Begin()
+    {
+        if (!isCountingDown)
+        {
+            isCountingDown = true;
+            timeRemaining = duration;
+            Invoke("_tick", 1f);
+        }
+    }
+
+    private void _tick()
+    {
+        timeRemaining--;
+        if (timeRemaining > 0)
+        {
+            Invoke("_tick", 1f);
+        }
+        else
+        {
+            isCountingDown = false;
+        }
+        timerText.text = timeRemaining.ToString();
+    }
+}
