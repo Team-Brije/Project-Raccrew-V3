e2021726379f9032cf633be5bcd7826bad6488a7
Shika <shikatastrophe@gmail.com>
2024-07-06 22:35:15 -0600

    YA SIRVEN LAS ESPLOSIONESSSSSS

---
+++ Assets/Scripts/ScriptableObjPowerUps/PowerUpLogic/Bullet.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject ExplosionPrefab;
+    public GameObject MainGameObject;
+    public float gravity;
+
+    public int bulletType; //0 = ground hitting, 1 = player hitting
+
+    public Vector3 P0, V0;
+
+    private float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke("Die",5); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        transform.position = positionFunction();
+        transform.forward = velocityFunction();
+    }
+
+    Vector3 positionFunction()
+    {
+        Vector3 g = new Vector3(0, gravity, 0);
+        return 0.5f * g * t * t + V0 * t + P0;
+    }
+
+    Vector3 velocityFunction()
+    {
+        Vector3 g = new Vector3(0, gravity, 0);
+        return g * t + V0;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Stage" && bulletType == 0)
+        {
+            Instantiate(ExplosionPrefab, gameObject.transform.position, Quaternion.identity);
+            Destroy(MainGameObject);
+        }
+        if (other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2" || other.gameObject.tag == "Player3" || other.gameObject.tag == "Player4" && bulletType == 1)
+        {
+            Instantiate(ExplosionPrefab, gameObject.transform.position, Quaternion.identity);
+            Destroy(MainGameObject);
+        }
+    }
+}
