46c00a00dcecb2b3c2f34952c5a993ac185b9198
Shika <shikatastrophe@gmail.com>
2024-07-10 15:35:25 -0600

    More Bug Fixes
    Wind now only pushes forward, as well as stuns a bit more, so it is still possible to pass through, but it requires more skill now

--- Assets/Scripts/ScriptableObjPowerUps/PowerUpLogic/Wind.cs
+++ Assets/Scripts/ScriptableObjPowerUps/PowerUpLogic/Wind.cs
@@ -11,30 +11,29 @@ public class Wind : MonoBehaviour
 
     private void Start()
     {
-        origin = gameObject.transform;
+        origin = transform;
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player1")
-        { 
-            other.GetComponent<Rigidbody>().AddExplosionForce(WindStrength * GameManager.DirtP1, gameObject.transform.position, 10, FloorUpwardsForce, ForceMode.Force);
+        {
+            other.GetComponent<Rigidbody>().AddForce((origin.forward * WindStrength) * GameManager.DirtP1, ForceMode.Force); 
             GameManager.StartStun(1,StunTime);
-            
         }
         if (other.gameObject.tag == "Player2")
         {
-            other.GetComponent<Rigidbody>().AddExplosionForce(WindStrength * GameManager.DirtP2, gameObject.transform.position, 10, FloorUpwardsForce, ForceMode.Force);
+            other.GetComponent<Rigidbody>().AddForce((origin.forward * WindStrength) * GameManager.DirtP2, ForceMode.Force);
             GameManager.StartStun(2, StunTime);
         }
         if (other.gameObject.tag == "Player3")
         {
-            other.GetComponent<Rigidbody>().AddExplosionForce(WindStrength * GameManager.DirtP3, gameObject.transform.position, 10, FloorUpwardsForce, ForceMode.Force);
+            other.GetComponent<Rigidbody>().AddForce((origin.forward * WindStrength) * GameManager.DirtP3, ForceMode.Force);
             GameManager.StartStun(3, StunTime);
         }
         if (other.gameObject.tag == "Player4")
         {
-            other.GetComponent<Rigidbody>().AddExplosionForce(WindStrength * GameManager.DirtP4, gameObject.transform.position, 10, FloorUpwardsForce, ForceMode.Force);
+            other.GetComponent<Rigidbody>().AddForce((origin.forward * WindStrength) * GameManager.DirtP4, ForceMode.Force);
             GameManager.StartStun(4, StunTime);
         }
     }
