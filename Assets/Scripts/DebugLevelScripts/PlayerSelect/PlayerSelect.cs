56e8647f357e798e7624dab5fe6b656fac97a9ff
Shika <shikatastrophe@gmail.com>
2024-08-02 20:48:25 -0600

    Mapache Escoba

--- Assets/Scripts/DebugLevelScripts/PlayerSelect/PlayerSelect.cs
+++ Assets/Scripts/DebugLevelScripts/PlayerSelect/PlayerSelect.cs
@@ -24,27 +24,27 @@ public class PlayerSelect : MonoBehaviour
             case 0:
                 break;
             case 1:
-                player = GameObject.FindGameObjectWithTag("Player1");
+                player = GameObject.FindGameObjectWithTag("Player1Cap");
                 player.GetComponent<MeshRenderer>().material = playerSkin.targetMaterial;
                 playerUI = GameObject.FindGameObjectWithTag("PlayerUI1");
                 playerUI.SetActive(false);
                 break;
             case 2:
-                player = GameObject.FindGameObjectWithTag("Player2");
+                player = GameObject.FindGameObjectWithTag("Player2Cap");
                 player.GetComponent<MeshRenderer>().material = playerSkin.targetMaterial;
                 playerUI = GameObject.FindGameObjectWithTag("PlayerUI2");
                 PlayerSelectScript.arePlayersReady = true;
                 playerUI.SetActive(false);
                 break;
             case 3:
-                player = GameObject.FindGameObjectWithTag("Player3");
+                player = GameObject.FindGameObjectWithTag("Player3Cap");
                 player.GetComponent<MeshRenderer>().material = playerSkin.targetMaterial;
                 playerUI = GameObject.FindGameObjectWithTag("PlayerUI3");
                 PlayerSelectScript.arePlayersReady = true;
                 playerUI.SetActive(false);
                 break;
             case 4:
-                player = GameObject.FindGameObjectWithTag("Player4");
+                player = GameObject.FindGameObjectWithTag("Player4Cap");
                 player.GetComponent<MeshRenderer>().material = playerSkin.targetMaterial;
                 playerUI = GameObject.FindGameObjectWithTag("PlayerUI4");
                 PlayerSelectScript.arePlayersReady = true;
