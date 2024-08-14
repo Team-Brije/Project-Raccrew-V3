7df1f57e99b0337871e2f5ea8549d5b498e3d164
Wilbert <114256884+warcilac9@users.noreply.github.com>
2024-08-13 22:07:25 -0600

    Some quickFixes to ghost summon powerup

--- Assets/Scripts/GameScripts/GhostPush.cs
+++ Assets/Scripts/GameScripts/GhostPush.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class GhostPush : MonoBehaviour
 {
     public float force;
+    public float StunTime;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2" || other.gameObject.tag == "Player3" || other.gameObject.tag == "Player4")
+       
+
+        if (other.gameObject.tag == "Player1")
         {
             Vector3 direction = (transform.position - other.transform.position).normalized;
-            other.gameObject.GetComponent<Rigidbody>().AddForce(direction * -force);
-            
+            other.gameObject.GetComponent<Rigidbody>().AddForce(direction * -force * GameManager.DirtP1);
+            GameManager.StartStun(1, StunTime);
+        }
+        if (other.gameObject.tag == "Player2")
+        {
+            Vector3 direction = (transform.position - other.transform.position).normalized;
+            other.gameObject.GetComponent<Rigidbody>().AddForce(direction * -force * GameManager.DirtP2);
+            GameManager.StartStun(2, StunTime);
+        }
+        if (other.gameObject.tag == "Player3")
+        {
+            Vector3 direction = (transform.position - other.transform.position).normalized;
+            other.gameObject.GetComponent<Rigidbody>().AddForce(direction * -force * GameManager.DirtP3);
+            GameManager.StartStun(3, StunTime);
+        }
+        if (other.gameObject.tag == "Player4")
+        {
+            Vector3 direction = (transform.position - other.transform.position).normalized;
+            other.gameObject.GetComponent<Rigidbody>().AddForce(direction * -force * GameManager.DirtP4);
+            GameManager.StartStun(4, StunTime);
         }
     }
 }
