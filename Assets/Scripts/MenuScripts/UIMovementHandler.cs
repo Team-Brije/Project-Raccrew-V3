32d7c4790b787277e1554f1bc5c941c1ff60a80d
Shika <shikatastrophe@gmail.com>
2025-01-10 21:50:42 -0600

    FINISHED NEW CHARACTER SELECT
    IT IS NOW EASIER TO ADD NEW THINGS TO THE CHARACTER SELECT, JUST ADD TO THE ARRAY IN THE RESPECTIVE PART
    
    Co-Authored-By: amauryxd <118020379+amauryxd@users.noreply.github.com>

--- Assets/Scripts/MenuScripts/UIMovementHandler.cs
+++ Assets/Scripts/MenuScripts/UIMovementHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMovementHandler : MonoBehaviour
 {
@@ -9,6 +10,15 @@ public class UIMovementHandler : MonoBehaviour
     bool confirmationBool;
     public float[] ypos;
     int arrpos;
+    float timer;
+    public MovementHandler movementHandler;
+    public GameObject playerUI;
+    public GameObject characterselectUI;
+    float fillamount;
+    public Image image;
+    float fullAmount;
+
+    public CharacterSelectItemStorage[] itemStorages;
     private void OnEnable()
     {
         InputHandler.OnUp += MoveUp;
@@ -26,28 +36,59 @@ public class UIMovementHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        confirmationBool = false;
+        fullAmount = 1.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlayerSelectScript.arePlayersReady = false;
         transform.localPosition = new Vector3(0, Mathf.Lerp(1, ypos[arrpos], 100),0);
+        if ( confirmationBool )
+        {
+            timer += Time.deltaTime;
+            fillamount = timer / fullAmount;
+
+            image.fillAmount = fillamount;
+            if ( timer >= fullAmount)
+            {
+                foreach (var item in itemStorages)
+                {
+                    item.SetAttribute();
+                }
+                movementHandler.canMove = true;
+                if (PlayerSelectScript.NumOfPlayers > 1) { PlayerSelectScript.arePlayersReady = true; }
+                playerUI.SetActive(false);
+                characterselectUI.SetActive(false);
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            timer = 0;
+            image.fillAmount = 0;
+        }
     }
 
     public void Confirm(int id, bool isConfirming)
     {
-
+        if(id == playerId)
+        {
+            confirmationBool = isConfirming;
+        }
     }
 
     public void MoveUp(int id)
     {
+        if (id != playerId) { return; }
         if (arrpos >= ypos.Length -1) { arrpos = 0; }
         else { arrpos++; } 
     }
 
     public void MoveDown(int id)
     {
+        if(id != playerId) { return; }
         if (arrpos <= 0) { arrpos = ypos.Length -1; }
         else { arrpos--; }
     }
--- Assets/Scripts/TEST SCRIPTS/AnimationPlayer.cs
+++ Assets/Scripts/TEST SCRIPTS/AnimationPlayer.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class AnimationPlayer : MonoBehaviour
 {
+    public enum ColliderOrientation {  Left, Right };
+    public ColliderOrientation orientation;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Object Entered!");
+        switch (orientation)
+        {
+            case ColliderOrientation.Left:
+                if(other.gameObject.TryGetComponent(out CharacterSelectItemStorage storage))
+                {
+                    storage.ChangeAttributeDown();
+                }
+                break;
+            case ColliderOrientation.Right:
+                if (other.gameObject.TryGetComponent(out CharacterSelectItemStorage storage1))
+                {
+                    storage1.ChangeAttributeUp();
+                }
+                break;
+        }
     }
 }
