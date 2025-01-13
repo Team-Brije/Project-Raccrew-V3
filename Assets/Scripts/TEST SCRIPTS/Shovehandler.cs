c34271274f568bb44ebcbdaec3467adc4956894e
Shika <shikatastrophe@gmail.com>
2025-01-12 21:03:22 -0600

    Added Shove animation and implemented

--- Assets/Scripts/TEST SCRIPTS/Shovehandler.cs
+++ Assets/Scripts/TEST SCRIPTS/Shovehandler.cs
@@ -7,12 +7,18 @@ public class Shovehandler : MonoBehaviour
     [Header("Event Listener Variables")]
     MovementHandler player;
     public int playerId;
+    public string AnimName;
+
 
     [Header("Shove Variables")]
     [HideInInspector] public bool isShoving = false;
     [HideInInspector] public bool isMoving;
     public float stunTime;
     public Collider shoveCollider;
+    public Animator animator;
+    public float ShoveCooldown;
+    bool canShove;
+
 
     private void OnEnable() {
         InputHandler.OnShove += Shove;
@@ -22,19 +28,30 @@ public class Shovehandler : MonoBehaviour
     }
     private void Start() {
         player = GetComponent<MovementHandler>();
+        animator = GetComponent<Animator>();
+        AnimName = "Shove";
         playerId = player.playerId;
+        canShove = true;
     }
     public void Shove(int id){
-        if(id==playerId){
+        if(id==playerId && canShove){
+            animator.Play(AnimName);
+            /*
             StartCoroutine(shoveActivator());
+            */
             StartCoroutine(lawbringer());
         }
     }
     public IEnumerator lawbringer()
     {
+        canShove = false;
+        yield return new WaitForSeconds(ShoveCooldown);
+        canShove=true;
+        /*
         isShoving = true;
         yield return new WaitForSeconds(1.6f);
         isShoving = false;
+        */
     }
     public void StartStun()
     {
